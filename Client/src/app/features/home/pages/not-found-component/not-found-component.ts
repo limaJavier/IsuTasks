3f4d619aebc8464fa6a7fb6a233e3627008693cf
Javier Lima <javi.limashelby@gmail.com>
2025-11-20 18:02:23 -0500

    feat[client]: implement not-found component

---
+++ Client/src/app/features/home/pages/not-found-component/not-found-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../../shared/components/header-component/header-component';
+
+@Component({
+  selector: 'app-not-found-component',
+  imports: [HeaderComponent],
+  templateUrl: './not-found-component.html',
+  styleUrl: './not-found-component.css',
+})
+export class NotFoundComponent {
+
+}
